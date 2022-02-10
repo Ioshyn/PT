6010c666cb1d24757df5be640b5242d737d0f925
Yasmine <99295205+Ioshyn@users.noreply.github.com>
2022-02-10 17:11:35 +0100

    Create fileRepo.py

---
+++ fileRepo.py
@@ -0,0 +1,7 @@
+class FileRepo():
+    
+    def exportDataString(self,Ltimes,Lpoints,delim): #
+        string = ""
+        for i in range (len(Ltimes)):
+            string = string  + str(Ltimes[i]) + delim + str(Lpoints[i].getX()) + delim + str(Lpoints[i].getY())+"\n"
+        return string
