b61d56b37120a335f18795cfd9f96d00391c0256
Yasmine <99295205+Ioshyn@users.noreply.github.com>
2022-02-16 22:40:27 +0100

    Update fileRepo.py

--- fileRepo.py
+++ fileRepo.py
@@ -5,6 +5,10 @@ class FileRepo():
         for i in range (len(Ltimes)):
             string = string  + str(Ltimes[i]) + delim + str(Lpoints[i].getX()) + delim + str(Lpoints[i].getY())+"\n"
         return string
+    
+    def exportDataToCsv(self,filename,string):
+         f = open("fichier.csv","w")
+         f.write(string)
 
 
     
